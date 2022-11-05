694a5a6c4f7a54f65bc479fc05458319634cc3c7
Mitchell Conrad <Mitchell.conrad@gmail.com>
2022-11-05 18:47:27 -0400

    Very basic pitch and roll

--- drone/drone.ino
+++ drone/drone.ino
@@ -13,6 +13,44 @@ int rearLeft   =  5;
 int rearRight  = 12; 
 // --------------------------------------------------------------------
 
+
+// pulseWidth takes motor, throttle, roll, pitch, yaw
+// We need to stop the pulsewidth from passing a certain value if the angle from the mpu is a certain angle
+int pulseWidth(int motor, int throttle, int roll, int pitch, int yaw){
+  // Throttle is 0-80
+  // Roll, pitch, yaw are -100 to 100
+
+  float response = 0.005;
+
+  float multiplier = 1.0;
+
+  // Calculate roll
+  // Roll right, power left
+  if (roll > 0 && (motor == rearLeft || motor == frontLeft)){
+    multiplier += (response * roll);
+  }
+  // Roll left, power right
+  else if (roll < 0 && (motor == rearRight || motor == frontRight)){
+    multiplier += (response * (-roll));
+  }
+
+  // Calculate pitch
+  // Pitch forward, power rear
+  if (pitch > 0 && (motor == rearRight || motor == rearLeft)){
+    multiplier += (response * pitch);
+  }
+  // Pitch back, power front
+  else if (pitch < 0 && (motor == frontLeft || motor == frontRight)){
+    multiplier += (response * (-pitch));
+  }
+
+  // Calculate yaw
+
+  // Combine calculations to create multiplier
+
+  return (int)(multiplier * throttle);
+}
+
 void setup() {
   // IBUS SETUP ------------------------------------------------------
   ibusRc.begin(ibusRcSerial);
@@ -31,8 +69,6 @@ void setup() {
   // MPU SETUP
 
   // ------------------------------------------------------------------
-
-
 }
 
 // READS DATA FROM THE IBUS -------------------------------------------
@@ -46,17 +82,25 @@ int readChannel(byte channelInput, int minLimit, int  maxLimit, int defaultValue
 
 void loop() {
   //READ DATA FROM IBUS AND SET PWM ----------------------------------
-  //CHANNLE 2 THROTTLE, CHANNEL 3 YAW, CHANNEL 0 ROLL, CHANNEL 1 PITCH 
-  int throttle = readChannel(2, 0, 100, 0);
-  int yaw = readChannel(3, 0, 100, 0);
-  int roll = readChannel(0, 0, 100, 0);
-  int pitch = readChannel(1, 0, 100, 0);
+  //CHANNEL 2 THROTTLE, CHANNEL 3 YAW, CHANNEL 0 ROLL, CHANNEL 1 PITCH 
+  int throttle = readChannel(2, 0, 80, 0);
+  int yaw = readChannel(3, -100, 100, 0);
+  int roll = readChannel(0, -100, 100, 0);
+  int pitch = readChannel(1, -100, 100, 0);
   
-  Serial.println(value);
-  SoftPWMSetPercent(frontLeft,  value);
-  SoftPWMSetPercent(rearLeft,   value);
-  SoftPWMSetPercent(frontRight, value);
-  SoftPWMSetPercent(rearRight , value);
+  //Serial.println(value);
+
+  // Calculate the pulse widths
+  int frontLeftPW = pulseWidth(frontLeft, throttle, roll, pitch, yaw);
+  int rearLeftPW = pulseWidth(rearLeft, throttle, roll, pitch, yaw);
+  int frontRightPW = pulseWidth(frontRight, throttle, roll, pitch, yaw);
+  int rearRightPW = pulseWidth(rearRight, throttle, roll, pitch, yaw);
+
+  // Update signals
+  SoftPWMSetPercent(frontLeft, frontLeftPW);
+  SoftPWMSetPercent(rearLeft, rearLeftPW);
+  SoftPWMSetPercent(frontRight, frontRightPW);
+  SoftPWMSetPercent(rearRight, rearRightPW);
   // ------------------------------------------------------------------
 
 }

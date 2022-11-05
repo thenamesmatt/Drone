ad95b17ee225d54c2c43bce3e7ec3eba5b62966f
mobrien13 <matthew.obrien5485@gmail.com>
2022-11-05 19:59:46 -0400

    Update drone.ino

--- drone/drone.ino
+++ drone/drone.ino
@@ -1,5 +1,7 @@
 #include <IBusBM.h>
 #include "SoftPWM.h"
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 #include <Wire.h>
 
 
@@ -13,6 +15,11 @@ int frontLeft  = 11;
 int frontRight = 13;
 int rearLeft   =  5;
 int rearRight  = 12; 
+// --------------------------------------------------------------------
+
+// GLOBAL VARIABLES FOR MPU ----------------------------------------
+Adafruit_MPU6050 mpu;
+
 // --------------------------------------------------------------------
 
 // pulseWidth takes motor, throttle, roll, pitch, yaw
@@ -61,9 +68,6 @@ int pulseWidth(int motor, int throttle, int roll, int pitch, int yaw){
   return (int)(multiplier * throttle);
 }
 
-// GLOBAL VARIABLES FOR MPU ----------------------------------------
-
-// --------------------------------------------------------------------
 
 void setup() {
   // IBUS SETUP ------------------------------------------------------
@@ -81,6 +85,28 @@ void setup() {
 
 
   // MPU SETUP --------------------------------------------------------
+  	Serial.begin(115200);
+
+	// Try to initialize!
+	if (!mpu.begin()) {
+		Serial.println("Failed to find MPU6050 chip");
+		while (1) {
+		  delay(10);
+		}
+	}
+	Serial.println("MPU6050 Found!");
+
+	// set accelerometer range to +-8G
+	mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+
+	// set gyro range to +- 500 deg/s
+	mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+
+	// set filter bandwidth to 21 Hz
+	mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+	delay(100);
+
 
   // ------------------------------------------------------------------
 }
@@ -120,6 +146,29 @@ void loop() {
 
 // READ DATA FROM MPU -----------------------------------------------
 
+	/* Get new sensor events with the readings */
+	sensors_event_t a, g, temp;
+	mpu.getEvent(&a, &g, &temp);
+
+	/* Print out the values */
+	Serial.print("Acceleration X: ");
+	Serial.print(a.acceleration.x);
+	Serial.print(", Y: ");
+	Serial.print(a.acceleration.y);
+	Serial.print(", Z: ");
+	Serial.print(a.acceleration.z);
+	Serial.println(" m/s^2");
+
+	Serial.print("Rotation X: ");
+	Serial.print(g.gyro.x);
+	Serial.print(", Y: ");
+	Serial.print(g.gyro.y);
+	Serial.print(", Z: ");
+	Serial.print(g.gyro.z);
+	Serial.println(" rad/s");
+
+	Serial.println("");
+  delay(1000);
 
 // ------------------------------------------------------------------
 
